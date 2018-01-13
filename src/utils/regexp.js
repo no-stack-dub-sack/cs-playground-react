2c7714a8055fb1ceb01a08184cc8c446532fa1c1
Peter Weinberg <pweinberg633@gmail.com>
2018-01-12 21:00:43 -0500

    add test report

--- src/utils/regexp.js
+++ src/utils/regexp.js
@@ -2,4 +2,4 @@
 export const re_resetState = new RegExp('resetState\\(\\)');
 export const re_SUPPRESS_TESTS = new RegExp('\\/\\/\\s\\s?SUPPRESS\\s\\s?TESTS', 'i');
 export const re_DO_NOT_SAVE = new RegExp('\\/\\/\\s\\s?DO\\s\\s?NOT\\s\\s?SAVE', 'i');
-export const re_errorTypes = new RegExp('WARNING:|Fail:|AssertionError|InternalError|RangeError|ReferenceError|EvalError|SyntaxError|TypeError|URIError');;
+export const re_errorTypes = new RegExp('tests failed|WARNING:|Fail:|AssertionError|InternalError|RangeError|ReferenceError|EvalError|SyntaxError|TypeError|URIError');;
--- src/utils/test/challenge/execute-tests.js
+++ src/utils/test/challenge/execute-tests.js
@@ -3,18 +3,20 @@
 function executeTests(tests) {
   if (tests) {
     console.log('\n/***** TESTS BEGIN *****/\n');
-
+    let numPassed = 0, numDisabled = 0;
     tests.forEach((test, i) => {
       try {
         // eslint-disable-next-line
         if (eval(test.expression) === 'DISABLED') {
           // throw error if test is disabled
+          numDisabled++;
           throw new Error('DISABLED');
         } else {
           // eslint-disable-next-line
           assert(eval(test.expression), test.message);
           // log passing test message
           console.log('Pass: ' + test.message);
+          numPassed++;
         }
       } catch (e) {
         switch (e.message) {
@@ -23,15 +25,21 @@ function executeTests(tests) {
             console.log('<code>' + e.message + ': ' + test.message + '</code>');
             break;
           case test.message:
-            // log failure message, plus any actual errors
-            console.log('Fail: ' + test.message + ' <code>[ ' + e.toString() + ' ]</code>');
-            break;
-          default:
             // log just failure message
             console.log('Fail: ' + test.message);
+            break;
+          default:
+            // log failure message, plus any actual errors
+            console.log('Fail: ' + test.message + ' <code>[ ' + e.toString() + ' ]</code>');
         }
       }
     });
+
+    console.log('\nREPORT:');
+    console.log('‾‾‾‾‾‾‾');
+    console.log(`- ${numPassed} out of ${tests.length} tests passed`);
+    console.log(`- ${tests.length - numPassed} out of ${tests.length} tests failed`);
+    console.log(`<code>- ${numDisabled} tests disabled (define method to enable)</code>`);
 
     console.log('\n/***** TESTS END *****/\n');
   }
