3db76f26b35e275782f3423ff130d48021c8203c
Peter Weinberg <pweinberg633@gmail.com>
2018-02-06 19:50:10 -0500

    fix(tests): flatten an array (#41)

--- src/assets/tests/algorithms/FlattenAnArray.js
+++ src/assets/tests/algorithms/FlattenAnArray.js
@@ -12,8 +12,8 @@ export const tests = [
     (() => {
       const __nested__ = [1, [2, 3, 4, 5], 2, 3, [4, 5, 6]]
       const result = '[1,2,3,4,5,2,3,4,5,6]'
-      return JSON.stringify(steamRoller(__nested_4__)) === result
-    })`,
+      return JSON.stringify(steamRoller(__nested__)) === result
+    })()`,
     message: 'The <code>steamRoller</code> function takes a two-dimensional array as an argument and returns a one-dimensional array containing all of the original arrays\' elements'
   },
   {
@@ -22,7 +22,7 @@ export const tests = [
       const __nested__ = [1, [2, 3, ['1', '2', '3'], true, false], null, 75, [24, 54, 62]]
       const result = '[1,2,3,"1","2","3",true,false,null,75,24,54,62]'
       return JSON.stringify(steamRoller(__nested__)) === result
-    })`,
+    })()`,
     message: 'The <code>steamRoller</code> function takes a three-dimensional array as an argument and returns a one-dimensional array containing all of the original arrays\' elements'
   },
   {
@@ -37,7 +37,7 @@ export const tests = [
       ]
       const result = '["deep",1,2,3,"four","fifty-five",2020,100,"whoah","ok",{"really?":"yes"},200,42,700,"700","deeper","no",null,null,"deepest-est?","nope!"]'
       return JSON.stringify(steamRoller(__nested__)) === result
-    })`,
+    })()`,
     message: 'The <code>steamRoller</code> function takes a nested array of arbitrary depth and returns a one-dimensional array containing all of the original arrays\' elements'
   }
 ];
