e00b6f21112f08881d2d22b1dff3ac39e4dca99c
Peter Weinberg <pweinberg633@gmail.com>
2018-02-08 21:56:49 -0500

    fix(loop-protect): take advantage of undocumented 2nd arg, remove hack (#47)

--- src/utils/test/challenge/loop-protect.js
+++ src/utils/test/challenge/loop-protect.js
@@ -1,31 +1,24 @@
+const Babel   = require('babel-standalone')
+const protect = require('loop-protect')
+
 // NOTE: needed to add extra helper before babel transform and check for infinite
 // loops first instead of calling loopProtect funct directly when code is eval'd.
 // When called directly it caused very strange behavior with loop-free code. But
 // when code is eval'd alone, works as expected, hence the extra step seen below.
 
-// use babel and loop-protect to transform user code
-// into protected loops. Instead of simply breaking the
-// loop, add regexp/replace logic to throw error & warn user
+const error = num => {
+  throw new Error(
+    `Timed out after 500ms due to infinite loop on line ${num}`
+  )
+}
 
-export const loopProtect = (code) => {
-  // choose arbitrarirly complex number
-  // to avoid accidental regexp matches
-  // & later replace with round number
-  const timeout = 500.235621347687320983287
-  const roundTo = Math.round(timeout)
-  const Babel = require('babel-standalone')
-  const protect = require('loop-protect')
-
-  Babel.registerPlugin('loopProtection', protect(timeout))
+const loopProtect = (code) => {
+  const timeout = 500
+  Babel.registerPlugin('loopProtection', protect(timeout, error))
   const transform = source => Babel.transform(source, {
     plugins: ['loopProtection'],
   }).code
-
-  const error = `Error('Timed out after ${roundTo}ms due to an infinite loop. Check your code and try again.')`
-  const _this = new RegExp(`> ${timeout}\\)\\sbreak;`, 'g')
-  const withThis = `> ${roundTo}) throw new ${error}`
-
-  return transform(code).replace(_this, withThis)
+  return transform(code)
 }
 
 // disable console during loop-protect to avoid
