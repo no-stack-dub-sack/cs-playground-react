1d887bfc3ca931f2ee018781b89e626808f781c6
Peter Weinberg <pweinberg633@gmail.com>
2018-02-11 14:03:53 -0500

    fix(loop-protect): add comments to code for extra clarity

--- src/utils/test/challenge/trim-code.js
+++ src/utils/test/challenge/trim-code.js
@@ -27,6 +27,7 @@
 */
 
 export default (code) => {
+  // arbitrarily complex markers to avoid replacing user code
   const ML_COMMENT_BEGIN = 'Ω≈≈__CS_PG_~_BEGIN_ML_~_CS_PG__≈≈Ω'
   const ML_COMMENT_BODY  = 'Ω≈≈__CS_PG_~_ML__BODY_~_CS_PG__≈≈Ω'
 
@@ -34,11 +35,13 @@ export default (code) => {
 
   for (let line = 0; line < code.length; line++) {
 
+    // handle  inline comments
     if (/\/\//.test(code[line])) {
       const index = code[line].indexOf('//')
       code[line] = code[line].slice(0, index)
     }
 
+    // handle /**/ comments on a single line
     if (/\/\*.*\*\//.test(code[line])) {
       const startIndex = code[line].indexOf('/*')
       const terminatorIndex = code[line].indexOf('*/')+1
@@ -46,21 +49,29 @@ export default (code) => {
         code[line].slice(terminatorIndex+1)
     }
 
+    // handle beginning of multi-line comments and mark spot
     if (/\/\*/.test(code[line]) && !/\*\//.test(code[line])) {
       code[line] = code[line].replace(/\/*\/.*/, ML_COMMENT_BEGIN)
     }
 
+    // if previous line is part of ml-comment
     if (
       code[line-1] === ML_COMMENT_BODY ||
       RegExp(ML_COMMENT_BEGIN).test(code[line-1])
     ) {
+        // if no terminator, mark as comment body
         if (!/\*\//.test(code[line])) {
           code[line] = ML_COMMENT_BODY
         } else {
+          // if comment ends on this line, remove
           code[line] = code[line].replace(/(\s*).*\*\//, '$1')
         }
     }
 }
+
+// filter off lines marked as comment body, join back into
+// code string, and replace multi-line comment beginning
+// markers to now have completely comment free code
 
 return code
   .filter(line => line !== ML_COMMENT_BODY)
